a27f5e48a531b8eb41a655ddb06a9d37e6410c3c
lucien-lefoll <lucien.lefoll@gmail.com>
2017-08-22 11:12:53 +0200

    Numeric pad and new layout

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ var app = new Vue({
     data: {
         expr: '',
         result: 0,
-        historic: []
+        historic: [],
+        inputs: [
+            [7, 8, 9],
+            [4, 5, 6],
+            [1, 2, 3],
+            ['+', 0, '-'],
+            ['*', '/', '^']
+        ]
     },
     methods: {
         calculate: function (){
             this.result = math.eval(this.expr)
             this.historic.push({calcul:this.expr, result:this.result})
+        },
+        append: function(input){
+            this.expr += input
         }
     }
 })
+
+Vue.config.devtools = true
