a2b126cf56d188d5770fdf779449e1c6a0ab0999
Nicolas Lacroix <NicoLacroix@users.noreply.github.com>
2017-08-22 11:00:03 +0200

    Add historic

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var app = new Vue({
     el: '#app',
     data: {
         expr: '',
-        result: 0
+        result: 0,
+        historic: [],
     },
     methods: {
         calculate: function (){
-            this.result = math.eval(this.expr)
+            this.result = math.eval(this.expr);
+            this.historic.push({calcul:this.expr, result:this.result});
         }
     }
 })
